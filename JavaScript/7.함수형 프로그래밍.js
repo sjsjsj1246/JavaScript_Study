9dd0c1bff7184b6f4c81093ba74064c25f4f040e
Hwang In Seo <sjsjsj1246@gmail.com>
2019-03-07 16:00:50 +0900

    js 공부
    7장 함수형 프로그래밍 완료

--- JavaScript/7.함수형 프로그래밍.js
+++ JavaScript/7.함수형 프로그래밍.js
@@ -270,13 +270,14 @@ console.log(new_func1(3));
 
 //범용적인 방법
 Function.prototype.curry = function () {
-    var fn = this.args = Array.prototype.slice.call(arguments);
+    var fn = this, args = Array.prototype.slice.call(arguments);
     return function () {
         return fn.apply(this, args.concat(Array.prototype.slice.call(arguments)));
     };
 };
 
-console.log(calculate.curry(1, 2).curry(3));
+func = calculate.curry(2, 3);
+console.log(func(4)); //10
 
 //calculate의 첫번째 인자와 세번째 인자만 고정하는법
 function curry2(func) {
@@ -295,3 +296,141 @@ var new_func = curry2(calculate, 1, undefined, 4);
 console.log(new_func(3)); //7
 
 //7.3.3 bind
+//curry 기법을 활용한 함수이다. js에 curry가 없는 이유가 bind함수로 충분히 대체가 가능하기 때문
+
+//기본적인 구현
+Function.prototype.bind = function (thisArg) {
+    var fn = this,
+        slice = Array.prototype.slice,
+        args = slice.call(arguments, 1);
+    return function () {
+        return fn.apply(thisArg, args.concat(slice.call(arguments)));
+    };
+}
+
+var print_all = function (arg) {
+    for (var i in this) console.log(i + " : " + this[i]);
+    for (var i in arguments) console.log(i + " : " + arguments[i]);
+}
+
+var myobj = { name: "zzoon" };
+
+var myfunc = print_all.bind(myobj);
+myfunc();
+
+var myfunc1 = print_all.bind(myobj, "iamhjoo", "others");
+myfunc1("insidejs");
+
+//7.3.4 래퍼
+//특정 함수를 자신의 함수로 덮어쓰는 것을 말한다. 오버라이드와 비슷
+
+function wrap(object, method, wrapper) {
+    var fn = object[method];
+    return object[method] = function () {
+        return wrapper.apply(this, [fn.bind(this)].concat(
+            Array.prototype.slice.call(arguments)));
+    };
+}
+
+Function.prototype.original = function (value) {
+    this.value = value;
+    console.log("value : " + this.value);
+}
+
+var mywrap = wrap(Function.prototype, "original", function (orig_func, value) {
+    this.value = 20;
+    orig_func(value);
+    console.log("wrapper value : " + this.value);
+});
+
+var obj = new mywrap("zzoon");
+
+//7.3.5 반복 함수
+
+//7.3.5.1 each
+/**
+ * 배열의 각 요소 혹은 객체의 각 프로퍼티를 하나씩 꺼내서 차례대로 특정 함수에
+ * 인자로 넣어 실행시키는 역할을 한다.
+ */
+
+function each(obj, fn, args) {
+    if (obj.length == undefined)
+        for (var i in obj)
+            fn.apply(obj[i], args || [i, obj[i]]);
+    else
+        for (var i = 0; i < obj.length; i++)
+            fn.apply(obj[i], args || [i, obj[i]]);
+    return obj;
+}
+
+each([1, 2, 3], function (idx, num) {
+    console.log(idx + ": " + num);
+});
+
+var zzoon = {
+    name: "zzoon",
+    age: 30,
+    sex: "Male"
+};
+
+each(zzoon, function (idx, value) {
+    console.log(idx + ": " + value);
+});
+
+//7.3.5.2 map
+/**
+ * 배열에서 많이 사용되는 함수이다. 배열의 각 요소를 꺼내서 사용자 정의 함수를 적용시켜
+ * 새로운 값을 얻은 후, 새로운 배열에 넣는다.
+ */
+
+Array.prototype.map = function (callback) {
+    /* this가 null인지, 배열인지 체크 */
+    /* callback이 함수이닞 체크 */
+
+    var obj = this;
+    var value, mapped_value;
+    var A = new Array(obj.length);
+
+    for (var i = 0; i < obj.length; i++) {
+        value = obj[i];
+        mapped_value = callback.call(null, value);
+        A[i] = mapped_value;
+    }
+
+    return A;
+}
+
+var arr = [1, 2, 3];
+var new_arr = arr.map(function (value) {
+    return value * value;
+});
+
+console.log(new_arr);
+
+//7.3.5.3 reduce
+/**
+ * reduce()는 배열의 각 요소를 하나씩 꺼내서 사용자의 함수를 적용시킨 뒤, 그 값을 계속해서
+ * 누적시키는 함수이다.
+ */
+
+Array.prototype.reduce = function (callback, memo) {
+    // this가 null인지, 배열인지 체크
+    // callback이 함수인지 체크
+
+    var obj = this;
+    var value, accumulated_value = 0;
+
+    for (var i = 0; i < obj.length; i++) {
+        value = obj[i];
+        accumulated_value = callback.call(null, accumulated_value, value);
+    }
+
+    return accumulated_value;
+};
+
+var arr = [1, 2, 3];
+var accumulated_value = arr.reduce(function (a, b) {
+    return a + b * b;
+});
+
+console.log(accumulated_value); //1*1+2*2+3*3 = 14
